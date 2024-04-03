fbcccaa8311c2f51d2a95218e55ccacb4a3785e8
AlfredoQuispeUbaldo <71438344@continental.edu.pe>
2024-04-03 18:33:48 -0500

    Se refactorizo las lineas de codigo ingreso de datos

--- main.py
+++ main.py
@@ -13,27 +13,24 @@ def mcd(a, b):
         a, b = b, a % b
     return a
 
+def solicitar_numero_positivo(mensaje):
+    while True:
+        try:
+            num = int(input(mensaje))
+            if num > 0:
+                return num
+            else:
+                print("Por favor, ingresa un número positivo.")
+        except ValueError:
+            print("Por favor, ingresa un número entero válido.")
+
 # Solicitar al usuario que ingrese el primer número positivo
-while True:
-    try:
-        num1 = int(input("Ingresa el primer número positivo: "))
-        if num1 > 0:
-            break
-        else:
-            print("Por favor, ingresa un número positivo.")
-    except ValueError:
-        print("Por favor, ingresa un número entero válido.")
+num1 = solicitar_numero_positivo("Ingresa el primer número positivo: ")
 
 # Solicitar al usuario que ingrese el segundo número positivo
-while True:
-    try:
-        num2 = int(input("Ingresa el segundo número positivo: "))
-        if num2 > 0:
-            break
-        else:
-            print("Por favor, ingresa un número positivo.")
-    except ValueError:
-        print("Por favor, ingresa un número entero válido.")
+num2 = solicitar_numero_positivo("Ingresa el segundo número positivo: ")
+
+print("Los números ingresados son:", num1, "y", num2)
 
 # Calcular el máximo común divisor y mostrar el resultado
 print("El máximo común divisor de", num1, "y", num2, "es:", mcd(num1, num2))
